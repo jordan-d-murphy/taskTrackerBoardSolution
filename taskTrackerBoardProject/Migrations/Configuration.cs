8128d2acb15ac979ec49b74277a5af36fa0bb6cf
Jordan Murphy <murphy2@iastate.edu>
2020-02-29 22:36:05 +0000

    Added CRUD to task, added search and filter by tag

---
+++ taskTrackerBoardProject/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace taskTrackerBoardProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<taskTrackerBoardProject.Models.TaskDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "taskTrackerBoardProject.Models.TaskDBContext";
+        }
+
+        protected override void Seed(taskTrackerBoardProject.Models.TaskDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ taskTrackerBoardProject/Models/Task.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace taskTrackerBoardProject.Models
+{
+    public class Task
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [Display(Name = "Date Created")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+
+        [Display(Name = "Target Due Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DueDate { get; set; }
+        //public List<Comment> Comments { get; set; } // all comments on this task with user, datetime, message
+        //public Theme CurrentTheme { get; set; } // current color theme for this tasks card, may come from tags, dropdown with all associated tags, or create custom
+        //public User CreatedBy { get; set; } // the user that created the task 
+        //public Status CurrentStatus { get; set; } // the status is the column that the task is currently in (to do, in progress, finished, etc)
+        public string Tag { get; set; } // all the current tags associated with this task, can set color theme, or filter board by tags
+
+    }
+
+    public class TaskDBContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
