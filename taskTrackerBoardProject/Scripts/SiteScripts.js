c9ee8b6e0b7cc0a4748403e5e1bd507c0eaa01bb
Jordan Murphy <murphy2@iastate.edu>
2020-03-01 09:06:00 +0000

    finished drag and drop, neet to set up post for update after drop

---
+++ taskTrackerBoardProject/Scripts/SiteScripts.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    console.log("Loaded site scripts for drag and drop!");
+    $(function () {
+        $(".draggable").draggable({
+            zIndex: 100
+        });
+        $(".droppable").droppable({
+            drop: function (event, ui) {
+                console.log("dragged element " + ui.draggable.attr("id") +
+                    " onto target droppable element " + $(this).attr("id") +
+                    " -> ready to pass to controller and reload page");
+
+                $.post('/Tasks/Edit',
+                    {
+                        ID: ui.draggable.attr("id"),
+                        CurrentStatus: $(this).attr("id")
+                    },
+                    function (data) {
+
+                    console.log("Here is the return value -> " + data);
+
+                });
+
+            }
+        });
+    });
+});
